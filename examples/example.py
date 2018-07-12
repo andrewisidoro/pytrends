f93834cd8cd6a33d74c885fa882aa88e87fb924f
andrewisidoro <andrew@seofosho.com>
2018-07-12 21:23:56 +0100

    Updated commenting

--- examples/example.py
+++ examples/example.py
@@ -12,21 +12,21 @@ interest_over_time_df = pytrend.interest_over_time()
 print(interest_over_time_df.head())
 
 # Interest by Region
-interest_by_region_df = pytrend.interest_by_region()
-print(interest_by_region_df.head())
+#interest_by_region_df = pytrend.interest_by_region()
+#print(interest_by_region_df.head())
 
 # Related Queries, returns a dictionary of dataframes
-related_queries_dict = pytrend.related_queries()
-print(related_queries_dict)
+#related_queries_dict = pytrend.related_queries()
+#print(related_queries_dict)
 
 # Get Google Hot Trends data
-trending_searches_df = pytrend.trending_searches()
-print(trending_searches_df.head())
+#trending_searches_df = pytrend.trending_searches()
+#print(trending_searches_df.head())
 
 # Get Google Top Charts
-top_charts_df = pytrend.top_charts(cid='actors', date=201611)
-print(top_charts_df.head())
+#top_charts_df = pytrend.top_charts(cid='actors', date=201611)
+#print(top_charts_df.head())
 
 # Get Google Keyword Suggestions
-suggestions_dict = pytrend.suggestions(keyword='pizza')
-print(suggestions_dict)
+#suggestions_dict = pytrend.suggestions(keyword='pizza')
+#print(suggestions_dict)
